7f992c4842957ebc3b5022e88dc600996022b975
silentnoname <44841346+silentnoname@users.noreply.github.com>
2022-06-03 00:37:11 +0800

    Add files via upload

---
+++ fortainstall.sh
@@ -0,0 +1,35 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo systemctl enable systemd-timesyncd
+sudo systemctl start systemd-timesyncd
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo cat <<'EOF' >>/etc/docker/daemon.json
+{
+   "default-address-pools": [
+        {
+            "base":"172.17.0.0/12",
+            "size":16
+        },
+        {
+            "base":"192.168.0.0/16",
+            "size":20
+        },
+        {
+            "base":"10.99.0.0/16",
+            "size":24
+        }
+    ]
+}
+EOF
+sudo systemctl restart docker
+sudo curl https://dist.forta.network/pgp.public -o /usr/share/keyrings/forta-keyring.asc -s
+echo 'deb [signed-by=/usr/share/keyrings/forta-keyring.asc] https://dist.forta.network/repositories/apt stable main' | sudo tee -a /etc/apt/sources.list.d/forta.list
+sudo apt-get update
+sudo apt-get install forta -y
+
