b5659bbbe61076ccd59b3da50875b43c4b0b3a3b
silentnoname <44841346+silentnoname@users.noreply.github.com>
2023-01-14 19:56:10 +0800

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,4 +1,9 @@
-sudo apt-get remove docker docker-engine docker.io;
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y;
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get update &&  sudo apt-get install docker-ce -y ;
+sudo apt update -y && sudo apt upgrade -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
